7a18804a0660737479f8f4d2094be2c9be12f5e4
Mahesh-rathod13 <mahesh.rathod@atomicloops.com>
2025-05-12 17:15:29 +0530

    fix : implement without using reduce funtion

--- ArraySum.js
+++ ArraySum.js
@@ -1,7 +1,14 @@
 const ArraySum = (numbers) =>{
-    return numbers.reduce((accumulator, currentValue)=>{
-        return accumulator + currentValue;
-    }, 0);
+    try {
+        let sum = 0;
+        for(let i=0;i<numbers.length;i++){
+            sum += numbers[i];
+        }
+        return sum;
+    } catch (error) {
+        console.log(error);
+        return;
+    }
 }
 
 export {
